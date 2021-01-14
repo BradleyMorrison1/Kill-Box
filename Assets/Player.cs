b979fb3af28a6f7c34611ef18d600bf4a4933b8a
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-01-14 15:10:12 -0400

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject respawnPoint;
+    Vector3 respawnPos;
+    public GameObject message;
+
+    private void Start()
+    {
+        respawnPos = respawnPoint.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("KillBox"))
+        {
+            gameObject.transform.position = respawnPos;
+            Debug.Log("Triggered");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            message.SetActive(false);
+        }
+    }
+}
